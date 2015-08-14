b2e870dc319149b707a9f074da976e68810f7ca5
Hung Vu <vuhung.gemtek@gmail.com>
2015-08-14 11:31:58 +0700

    dragable

--- app/src/main/java/com/example/hungvu/testpack/MainActivity.java
+++ app/src/main/java/com/example/hungvu/testpack/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.hungvu.testpack;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private static final String IMAGEVIEW_TAG = "aaaaaa";
+    private ImageView i3;
+    private ImageView i1;
+    private ImageView i2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        i1 = (ImageView) findViewById(R.id.imageView);
+        i2 = (ImageView) findViewById(R.id.imageView2);
+        i3 = (ImageView) findViewById(R.id.imageView3);
+
+        // Sets the tag
+        i1.setTag(IMAGEVIEW_TAG);
+        i1.setOnTouchListener(this);
+
     }
 
     @Override
@@ -33,5 +52,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                // create it from the object's tag
+                ClipData.Item item = new ClipData.Item((CharSequence)view.getTag());
+
+                String[] mimeTypes = { ClipDescription.MIMETYPE_TEXT_PLAIN };
+                ClipData data = new ClipData(view.getTag().toString(), mimeTypes, item);
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+
+                view.startDrag( data, //data to be dragged
+                        shadowBuilder, //drag shadow
+                        view, //local data about the drag and drop operation
+                        0   //no needed flags
+                );
+
+                // hide but keep bound of view
+                view.setVisibility(View.INVISIBLE);
+                return true;
+        }
+        return false;
     }
 }
