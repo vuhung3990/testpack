88b5b6b2873fe65dcebd69316ac8ae940226bd8e
Hung Vu <vuhung.gemtek@gmail.com>
2015-08-14 14:42:06 +0700

    understand life cycle of drag and drop, pass data object between start and drop events

--- app/src/main/java/com/example/hungvu/testpack/MainActivity.java
+++ app/src/main/java/com/example/hungvu/testpack/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.ClipData;
 import android.content.ClipDescription;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     private ImageView i3;
     private ImageView i1;
     private ImageView i2;
+    private LinearLayout drop_container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +27,100 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         setContentView(R.layout.activity_main);
 
         i1 = (ImageView) findViewById(R.id.imageView);
-        i2 = (ImageView) findViewById(R.id.imageView2);
-        i3 = (ImageView) findViewById(R.id.imageView3);
 
         // Sets the tag
         i1.setTag(IMAGEVIEW_TAG);
         i1.setOnTouchListener(this);
 
+        drop_container = (LinearLayout) findViewById(R.id.drop_container);
+        drop_container.setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View view, DragEvent event) {
+                switch (event.getAction()) {
+                    // start
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        Log.d("life", "started");
+                        break;
+
+                    // start sending location and when user hover
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        Log.d("life", "entered");
+                        break;
+
+                    // get x, y here
+                    case DragEvent.ACTION_DRAG_LOCATION:
+                        Log.d("life", "X=" + event.getX() + ", Y=" + event.getY());
+                        break;
+
+                    // stop sending location if leave view
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        Log.d("life", "exited");
+                        break;
+
+                    // x, y last change here
+                    case DragEvent.ACTION_DROP:
+                        // retrieve object state here
+                        TagObject object = (TagObject) event.getLocalState();
+
+                        ImageView imageView = new ImageView(drop_container.getContext());
+                        imageView.setImageDrawable(i1.getDrawable());
+                        drop_container.addView(imageView);
+                        Log.d("life", "drop x=" + event.getX() + ", y=" + event.getY()+ ", width="+object.getWidth()+", height="+object.getHeight());
+                        break;
+
+                    // end
+                    case DragEvent.ACTION_DRAG_ENDED:
+
+                        // show for test
+                        i1.setVisibility(View.VISIBLE);
+                        Log.d("life", "ended");
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    class TagObject{
+        private int width;
+        private int height;
+
+        public TagObject(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        /**
+         * @return width
+         */
+        public int getWidth() {
+            return width;
+        }
+
+        /**
+         * set width
+         *
+         * @param width
+         */
+        public void setWidth(int width) {
+            this.width = width;
+        }
+
+        /**
+         * @return height
+         */
+        public int getHeight() {
+            return height;
+        }
+
+        /**
+         * set height
+         *
+         * @param height
+         */
+        public void setHeight(int height) {
+            this.height = height;
+        }
     }
 
     @Override
@@ -56,22 +147,25 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     @Override
     public boolean onTouch(View view, MotionEvent event) {
-        switch (event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 // create it from the object's tag
-                ClipData.Item item = new ClipData.Item((CharSequence)view.getTag());
+                ClipData.Item item = new ClipData.Item((CharSequence) view.getTag());
 
-                String[] mimeTypes = { ClipDescription.MIMETYPE_TEXT_PLAIN };
+                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
                 ClipData data = new ClipData(view.getTag().toString(), mimeTypes, item);
                 View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
 
-                view.startDrag( data, //data to be dragged
+                // create local state object then retrieve later in ACTION_DROP
+                TagObject tagObject = new TagObject(2,1);
+
+                view.startDrag(data, //data to be dragged
                         shadowBuilder, //drag shadow
-                        view, //local data about the drag and drop operation
+                        tagObject, //local data about the drag and drop operation
                         0   //no needed flags
                 );
 
-                // hide but keep bound of view
+                // hide but keep view placeholder
                 view.setVisibility(View.INVISIBLE);
                 return true;
         }
