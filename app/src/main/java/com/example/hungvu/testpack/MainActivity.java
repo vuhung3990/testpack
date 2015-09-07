d841a7ad275bfb67015c6254eb4d195ce999a42e
Hung Vu <vuhung.gemtek@gmail.com>
2015-09-07 14:09:47 +0700

    text input layout and error handle

--- app/src/main/java/com/example/hungvu/testpack/MainActivity.java
+++ app/src/main/java/com/example/hungvu/testpack/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example.hungvu.testpack;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private TextInputLayout userNameContainer;
+    private EditText userNameTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userNameContainer = (TextInputLayout) findViewById(R.id.userName);
+        userNameTxt = (EditText) findViewById(R.id.txtUserName);
+
+        // set error when text change
+        userNameTxt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus && userNameTxt.getText().toString().length() < 10){
+                    userNameContainer.setErrorEnabled(true);
+                    userNameContainer.setError("min 10 character.");
+                }else {
+                    userNameContainer.setError(null);
+                    userNameContainer.setErrorEnabled(false);
+                }
+            }
+        });
     }
 
     @Override
