b1129a27752f48b417e29b3f8ece42c20b6ebe17
Hung Vu <vuhung.gemtek@gmail.com>
2015-09-21 16:07:50 +0700

    fullscreen ads demo

--- app/src/main/java/com/example/hungvu/testpack/MainActivity.java
+++ app/src/main/java/com/example/hungvu/testpack/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.hungvu.testpack;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 
 public class MainActivity extends AppCompatActivity {
+    private Context context;
+    private InterstitialAd mInterstitialAd;
+
+    // 1. add gradle: compile 'com.google.android.gms:play-services:7.8.0'
+    // 2. edit AndroidManifest:
+    // 3. see example code
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-9624182208928945/2006736115");
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Toast.makeText(context, "close", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdLoaded() {
+                Toast.makeText(context, "loaded", Toast.LENGTH_SHORT).show();
+                mInterstitialAd.show();
+            }
+        });
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("A0FC1DE44D2B7B3F04613FDDC5D57FC6")
+                .addKeyword("game")
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
     }
 
     @Override
