7efc6fbae23a2ecb0459b165999cfa9fd0d6070e
Hung Vu <vuhung.gemtek@gmail.com>
2015-09-26 10:22:29 +0700

    float button beetween 2 view

--- app/src/main/java/com/example/hungvu/testpack/MainActivity.java
+++ app/src/main/java/com/example/hungvu/testpack/MainActivity.java
@@ -1,16 +1,42 @@
 package com.example.hungvu.testpack;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton actionButton;
+    private View anchor;
+
+    // 1. float layout in CoordinatorLayout or FrameLayout
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        anchor = (View) findViewById(R.id.anchor);
+
+        actionButton  = (FloatingActionButton) findViewById(R.id.float_button);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ObjectAnimator.ofFloat(actionButton, "rotation", 0, -90).setDuration(500).start();
+
+                ObjectAnimator moveX = ObjectAnimator.ofFloat(actionButton, "x", 240).setDuration(1000);
+                moveX.setStartDelay(500);
+                moveX.start();
+
+                ObjectAnimator moveY = ObjectAnimator.ofFloat(actionButton, "y", 120).setDuration(1000);
+                moveY.setStartDelay(500);
+                moveY.start();
+            }
+        });
+
     }
 
     @Override
